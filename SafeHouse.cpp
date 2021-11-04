18147c9206cd41955607f556162dbf6c3e30ee22
Evan Hanzelman <ehanz1997@gmail.com>
2021-11-04 12:34:17 -0400

    yeet

--- SafeHouse.cpp
+++ SafeHouse.cpp
@@ -1,42 +1,59 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+using namespace std;
 
 /*
 * Program uses manhattan distance to find the longest travel time
 * for a spy to a safe house.
 */
 
+int d (pair <int, int> s, pair<int, int> h);
+
 int main ()
 {
-    int n;
-    char fill;
-    int high_val;
+  int n = 0;
+  std :: cin >> n;
+  
+  std :: vector <pair <int, int>> spy;
+  std :: vector <pair <int, int>> house;
 
-    std :: cin >> n;
-    char grid [n][n];
-
-    std::vector<std::tuple <int, int>>  tuples;
-
-
-    for (int i = 0; i < n; i++)
+  for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
-        {
-            std :: cin >> fill;
-            grid [i][j] = fill;
-        }
+      for (int j = 0; j < n; j++)
+	{
+	  char temp;
+	  std :: cin >> temp;
+	  // takes user input and assigns the S and H values into separate grids
+	  if (temp == 'S')
+	    {
+	      spy.push_back ({i, j});
+	    }
+	  else if (temp == 'H')
+	    {
+	      house.push_back ({i, j});
+	    }
+	}
     }
 
-    /*
-    * Block a Safe House is on is denoted with H
-    * Block a Spy is on is denoted with S
-    * Regular block denoted with .
-    */
+  int result = 0;
+  for (int i = 0; i <spy.size (); i++)
+    {
+      int lim = 100000;
+      for (int j = 0; j < house.size (); j++)
+	{
+	  //lim equals the lesser of the min values
+	  lim = min (lim, d(spy[i],house[j]));
+	}
+      result = max (result, lim);
+    }
 
-   for (int i = 0; i < n; i++)
-   {
+  std :: cout << result << std :: endl;
+}
 
-   }
-
+int
+d (pair <int, int> s, pair <int, int> h)
+{
+  int r = abs (s.first - h.first) + abs (s.second - h.second);
+  return r;
 }
